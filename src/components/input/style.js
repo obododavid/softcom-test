9dfcd5e4f9b5f1bb6eb8cfde587197aa451fec9a
David Obodo <obododavid5@gmail.com>
2020-05-05 08:26:21 +0100

    change input border color to blue on focus and edit related styles

--- src/components/input/style.js
+++ src/components/input/style.js
@@ -1,22 +1,31 @@
 import styled from 'styled-components';
 
 export const Inputcontainer = styled.div`
-    border: 1px solid #f2f4f7;
-    border-color: rgba(10,46,101,.2);
-    border-radius: 5px;
-    padding: 15px;
     position: relative;
 
     input{
-        border: none;
+        border: 1px solid #f2f4f7;
+        border-color: rgba(10,46,101,.2);
+        border-radius: 5px;
+        padding: 15px;
         font-size: 14px;
         width: 100%;
         outline: transparent;
+        transition: 0.5s ease-in-out;
+        transition-property: color;
         color: #0a2e65;
 
         ::placeholder{
             color: #0a2e65;
             opacity: 0.6;
+        }
+
+        &:focus{
+            border-color: #0066f5;
+        }
+
+        &:focus + label{
+            color: #0066f5;
         }
     }
 
@@ -27,9 +36,10 @@ export const Inputcontainer = styled.div`
         font-size: 12px;
         padding: 5px 5px 0 5px;
         background-color: #ffffff;
-        color: rgba(0,0,0,0.4);
+        color: rgba(10, 46, 101, 0.5);
         opacity: 0;
-        transition: all 0.5s ease-in-out;
+        transition:0.5s ease-in-out;
+        transition-property: top, opacity;
 
         ${({ focus }) => focus && 'top: -12px; opacity: 1'};
     }

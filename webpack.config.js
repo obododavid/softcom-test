230c80f43b6a3b54720e6b9385dcb950790bd7f7
David Obodo <obododavid5@gmail.com>
2020-05-05 03:18:15 +0100

    -set up basic webpack config -set up start script

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const mode = process.env.NODE_ENV || development;
+
+module.exports = {
+    mode: mode,
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[contenthash].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 3000,
+        compress: true
+    },
+    devtool: (mode === 'development') ? 'eval-cheap-module-source-map' : 'source-map'
+}

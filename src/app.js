e86cb5fa645ca97914f87ce5192177af836d6150
David Obodo <obododavid5@gmail.com>
2020-05-05 07:09:17 +0100

    -create form component -inject globalstyles into project

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { GlobalStyles } from './globalStyles';
+import Form from './components/form/form';
 
 const App = () => {
     return (
         <div>
+            <GlobalStyles />
             App component
+            <Form />
         </div>
     )
 }
---
+++ src/components/form/form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FormContainer } from './style';
+
+const Form = () => {
+    return (
+        <FormContainer>
+            This is the form
+        </FormContainer>
+    )
+}
+
+export default Form;

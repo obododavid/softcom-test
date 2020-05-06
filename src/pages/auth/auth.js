86d59254219da935e35293fb9db63e9850c628e3
David Obodo <obododavid5@gmail.com>
2020-05-06 02:36:41 +0100

    add little header for form

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -5,15 +5,35 @@ import styled from 'styled-components';
 const AuthContainer = styled.div`
     display: flex;
     justify-content: center;
+    flex-direction: column;
     height: 100vh;
     min-height: 800px;
     align-items: center;
     background-color: #f4f7fa;
+
+    header{
+        color: black;
+        margin-bottom: 10px;
+        font-weight: 400;
+        color: #0297CD;
+        font-size: 14px;
+
+        span{
+            font-style: italic;
+            font-size: 30px;
+            font-weight: 600;
+
+            span{
+                color: #FFB700;
+            }
+        }
+    }
 `
 
 const Auth = () => {
     return (
         <AuthContainer>
+            <header><span>Softc<span>o</span>m</span> Front end Assessment</header>
             <Form />
         </AuthContainer>
     )
